d6cef3ae9a632f13ed411adbc80e25aa773f2222
panlong <1170468846@qq.com>
2023-12-12 10:30:26 +0800

    修复bug

--- uniplugin_electronic/src/main/java/com/example/uniplugin_electronic/Electronic.java
+++ uniplugin_electronic/src/main/java/com/example/uniplugin_electronic/Electronic.java
@@ -1,7 +1,9 @@
 package com.example.uniplugin_electronic;
 
 import android.content.Context;
+import android.os.Handler;
 import android.os.RemoteException;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -9,122 +11,223 @@ import com.alibaba.fastjson.JSONObject;
 import com.sunmi.scalelibrary.ScaleManager;
 import com.sunmi.scalelibrary.ScaleResult;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import io.dcloud.feature.uniapp.bridge.UniJSCallback;
 
 public class Electronic {
 
+    private static final String TAG = "Electronic";
     Context mContext;
     private ScaleManager scaleManager;
     private ScaleManager.ScaleServiceConnection scaleServiceConnection;
 
-    public Electronic(@NonNull Context context) {
+    // 添加一个标志来指示连接状态
+    private boolean isServiceConnected = false;
+
+    public Electronic(@NonNull Context context, final UniJSCallback jsCallback) {
         mContext = context;
         scaleManager = ScaleManager.getInstance(context);
+
+        Log.e(TAG, "scaleManager");
+
         scaleServiceConnection = new ScaleManager.ScaleServiceConnection() {
             @Override
             public void onServiceConnected() {
+                Log.e(TAG, "onServiceConnected");
+                // 设置连接状态为true
+                isServiceConnected = true;
+                if (jsCallback != null) {
+                    JSONObject data = new JSONObject();
+                    data.put("code", "初始化连接成功");
+                    jsCallback.invoke(data);
+                }
             }
 
             @Override
             public void onServiceDisconnect() {
+                Log.e(TAG, "onServiceDisconnect");
+                // 设置连接状态为false
+                isServiceConnected = false;
+                if (jsCallback != null) {
+                    JSONObject data = new JSONObject();
+                    data.put("code", "断开连接");
+                    jsCallback.invoke(data);
+                }
             }
         };
+
+        // 明确调用 connectService 方法
+        scaleManager.connectService(scaleServiceConnection);
+
+        Log.e(TAG, "电子称未连接");
+
+        // 添加一个延迟以确保 onServiceConnected 方法执行
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 在这里检查连接状态
+                checkConnectionStatus(jsCallback);
+            }
+        }, 6000); // 2000毫秒延迟示例
+
     }
 
+    // 在服务连接成功后检查连接状态
+    private void checkConnectionStatus(UniJSCallback jsCallback) {
+        if (!isServiceConnected) {
+            Log.e(TAG, "电子称未连接");
+            if (jsCallback != null) {
+                JSONObject data = new JSONObject();
+                data.put("code", "初始化连接失败");
+                jsCallback.invoke(data);
+            }
+        }
+    }
+
+
+    // 处理未连接错误
+    private void handleNotConnectedError(UniJSCallback jsCallback) {
+        Log.e(TAG, "电子称未连接");
+        if (jsCallback != null) {
+            JSONObject data = new JSONObject();
+            data.put("code", "电子称未连接");
+            jsCallback.invoke(data);
+        }
+    }
 
 
     //获取称重信息
 
-    public JSONObject getResult() throws RemoteException {
-        JSONObject data = new JSONObject();
+    public void getResult(final UniJSCallback jsCallback) {
+        try {
+            scaleManager.getData(new ScaleResult() {
+                @Override
+                public void getResult(int net, int tare, boolean isStable) {
+                    JSONObject data = new JSONObject();
+                    data.put("net", net);
+                    data.put("tare", tare);
+                    data.put("isStable", isStable);
+                    if (jsCallback != null) {
+                        //这里返回称重结果
+                        jsCallback.invoke(data);
+                    }
+                }
 
-        scaleManager.getData(new ScaleResult() {
+                @Override
+                public void getStatus(boolean isLightWeight, boolean overload, boolean clearZeroErr, boolean calibrationErr) {
 
-            @Override
-            public void getResult(int net, int tare, boolean isStable) {
-                //这里返回称重结果
-                data.put("net", net);
-                data.put("tare", tare);
-                data.put("isStable", isStable);
-            }
+                }
 
-            @Override
-            public void getStatus(boolean isLightWeight, boolean overload, boolean clearZeroErr, boolean calibrationErr) {
+                @Override
+                public void getPrice(int net, int tare, int unit, String unitPrice, String totalPrice, boolean isStable, boolean isLightWeight) {
 
-            }
+                }
 
-            @Override
-            public void getPrice(int net, int tare, int unit, String unitPrice, String totalPrice, boolean isStable, boolean isLightWeight) {
+            });
 
-            }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // 在这里检查连接状态
+                    handleNotConnectedError(jsCallback);
+                }
+            }, 6000); // 2000毫秒延迟示例
 
-        });
-        return data;
+        } catch (RemoteException e) {
+            handleNotConnectedError(jsCallback);
+        }
     }
     //获取称重状态
 
-    public JSONObject getStatus() throws RemoteException {
-        JSONObject data = new JSONObject();
+    public void getStatus(final UniJSCallback jsCallback) {
+        try {
+            scaleManager.getData(new ScaleResult() {
 
-        scaleManager.getData(new ScaleResult() {
+                @Override
+                public void getResult(int net, int tare, boolean isStable) {
 
-            @Override
-            public void getResult(int net, int tare, boolean isStable) {
+                }
 
-            }
+                @Override
+                public void getStatus(boolean isLightWeight, boolean overload, boolean clearZeroErr, boolean calibrationErr) {
+                    JSONObject data = new JSONObject();
+                    //这里返回称重状态
+                    data.put("isLightWeight", isLightWeight);
+                    data.put("overload", overload);
+                    data.put("clearZeroErr", clearZeroErr);
+                    data.put("calibrationErr", calibrationErr);
+                    if (jsCallback != null) {
+                        //这里返回称重结果
+                        jsCallback.invoke(data);
+                    }
+                }
 
-            @Override
-            public void getStatus(boolean isLightWeight, boolean overload, boolean clearZeroErr, boolean calibrationErr) {
-                //这里返回称重状态
-                data.put("isLightWeight", isLightWeight);
-                data.put("overload", overload);
-                data.put("clearZeroErr", clearZeroErr);
-                data.put("calibrationErr", calibrationErr);
+                @Override
+                public void getPrice(int net, int tare, int unit, String unitPrice, String totalPrice, boolean isStable, boolean isLightWeight) {
 
-            }
+                }
 
-            @Override
-            public void getPrice(int net, int tare, int unit, String unitPrice, String totalPrice, boolean isStable, boolean isLightWeight) {
+            });
 
-            }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // 在这里检查连接状态
+                    handleNotConnectedError(jsCallback);
+                }
+            }, 6000); // 2000毫秒延迟示例
 
-        });
-        return data;
-
+        } catch (RemoteException e) {
+            handleNotConnectedError(jsCallback);
+        }
     }
 
     //获取称重状态
-    public JSONObject getPrice() throws RemoteException {
-        JSONObject data = new JSONObject();
-
-        scaleManager.getData(new ScaleResult() {
-
-            @Override
-            public void getResult(int net, int tare, boolean isStable) {
-
-            }
-
-            @Override
-            public void getStatus(boolean isLightWeight, boolean overload, boolean clearZeroErr, boolean calibrationErr) {
-
-            }
-
-            @Override
-            public void getPrice(int net, int tare, int unit, String unitPrice, String totalPrice, boolean isStable, boolean isLightWeight) {
-                //这里返回计价结果
-                data.put("net", net);
-                data.put("tare", tare);
-                data.put("unit", unit);
-                data.put("unitPrice", unitPrice);
-                data.put("totalPrice", totalPrice);
-                data.put("isStable", isStable);
-                data.put("isLightWeight", isLightWeight);
-            }
+    public void getPrice(final UniJSCallback jsCallback) {
 
 
-        });
+        try {
+            scaleManager.getData(new ScaleResult() {
 
-        return data;
+                @Override
+                public void getResult(int net, int tare, boolean isStable) {
+
+                }
+
+                @Override
+                public void getStatus(boolean isLightWeight, boolean overload, boolean clearZeroErr, boolean calibrationErr) {
+
+                }
+
+                @Override
+                public void getPrice(int net, int tare, int unit, String unitPrice, String totalPrice, boolean isStable, boolean isLightWeight) {
+                    JSONObject data = new JSONObject();
+                    //这里返回计价结果
+                    data.put("net", net);
+                    data.put("tare", tare);
+                    data.put("unit", unit);
+                    data.put("unitPrice", unitPrice);
+                    data.put("totalPrice", totalPrice);
+                    data.put("isStable", isStable);
+                    data.put("isLightWeight", isLightWeight);
+                    if (jsCallback != null) {
+                        //这里返回称重结果
+                        jsCallback.invoke(data);
+                    }
+                }
+
+            });
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // 在这里检查连接状态
+                    handleNotConnectedError(jsCallback);
+                }
+            }, 6000); // 2000毫秒延迟示例
+
+        } catch (RemoteException e) {
+            handleNotConnectedError(jsCallback);
+        }
 
     }
 
--- uniplugin_electronic/src/main/java/com/example/uniplugin_electronic/ElectronicModule.java
+++ uniplugin_electronic/src/main/java/com/example/uniplugin_electronic/ElectronicModule.java
@@ -15,25 +15,10 @@ public class ElectronicModule extends UniModule {
 
     private Electronic electronic;
 
-    @UniJSMethod(uiThread = true)
-    public void add(JSONObject json, UniJSCallback callback){
-        final int a = json.getIntValue("a");
-        final int b = json.getIntValue("b");
-        callback.invoke(new JSONObject(){{
-            put("code",200);
-            put("result",a+b);
-        }});
-    }
-
     //连接服务
     @UniJSMethod (uiThread = true)
-    public void init(UniJSCallback callback){
-        electronic = new Electronic(mUniSDKInstance.getContext());
-        if(callback != null) {
-            JSONObject data = new JSONObject();
-            data.put("code","初始化成功");
-            callback.invoke(data);
-        }
+    public void connectService(UniJSCallback callback){
+        electronic = new Electronic(mUniSDKInstance.getContext(),callback);
     }
 
 
@@ -41,31 +26,20 @@ public class ElectronicModule extends UniModule {
     @UniJSMethod (uiThread = true)
     public void getResult(UniJSCallback callback) throws RemoteException {
         Log.e(TAG, "getResult");
-
-        JSONObject result = electronic.getResult();
-        if(callback != null) {
-            callback.invoke(result);
-        }
-
+        electronic.getResult(callback);
     }
     //获取称重状态
     @UniJSMethod (uiThread = true)
     public void getStatus(UniJSCallback callback) throws RemoteException {
         Log.e(TAG, "getStatus");
-        JSONObject result = electronic.getStatus();
-        if(callback != null) {
-            callback.invoke(result);
-        }
+        electronic.getStatus(callback);
     }
 
     //获取称重状态
     @UniJSMethod (uiThread = true)
     public void getPrice(UniJSCallback callback) throws RemoteException {
         Log.e(TAG, "getPrice");
-        JSONObject result = electronic.getPrice();
-        if (callback != null) {
-            callback.invoke(result);
-        }
+        electronic.getPrice(callback);
     }
 
 }
